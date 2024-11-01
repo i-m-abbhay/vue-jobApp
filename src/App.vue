e6519ba3ac967e5922b20ae88a49e8b46d8f4b03
i-m-abbhay <20je0018@me.iitism.ac.in>
2024-11-01 17:10:03 +0900

    Forms and v-model

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { ref } from "vue";
 const name = ref("John Doe");
 const status = ref("pending");
 const tasks = ref(["Task 1", "Task 2", "Task 3"]);
+const newTask = ref("");
 
 const toggleStatus = () => {
   if (status.value === "active") {
@@ -14,6 +15,13 @@ const toggleStatus = () => {
     status.value = "active";
   }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
 </script>
 <template>
   <h1>Vue Jobs</h1>
@@ -22,6 +30,13 @@ const toggleStatus = () => {
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input id="newTask" v-model="newTask" type="text" name="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
     <li v-for="tas in tasks" :key="tas">{{ tas }}</li>
