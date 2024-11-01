9e0a17e61ee05a95e85ba38d65d24b41d54aad0f
i-m-abbhay <20je0018@me.iitism.ac.in>
2024-11-01 16:48:51 +0900

    v-for and v-bind directive and removed some default styles

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ export default {
     return {
       name: "John Doe",
       status: "pending",
+      tasks: ["Task 1", "Task 2", "Task 3"],
+      link: "https://www.google.com",
     };
   },
 };
@@ -15,4 +17,12 @@ export default {
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="tas in tasks" :key="tas">{{ tas }}</li>
+  </ul>
+
+  <!-- Any of the below two works -->
+  <!-- <a v-bind:href="link">Google</a> -->
+  <a :href="link">Google</a>
 </template>
