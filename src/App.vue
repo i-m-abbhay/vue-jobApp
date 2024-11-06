c9cdef31cd4de6cf88788b5b8ba562a25c4b44f6
i-m-abbhay <20je0018@me.iitism.ac.in>
2024-11-06 16:32:41 +0900

    computed function and truncated strings

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,15 @@
 <script setup>
-import { onMounted, ref } from "vue";
-
-const name = ref("John Doe");
-const status = ref("pending");
-const tasks = ref(["Task 1", "Task 2", "Task 3"]);
-const newTask = ref("");
-
-const toggleStatus = () => {
-  if (status.value === "active") {
-    status.value = "inactive";
-  } else if (status.value === "inactive") {
-    status.value = "pending";
-  } else {
-    status.value = "active";
-  }
-};
-
-const addTask = () => {
-  if (newTask.value.trim() !== "") {
-    tasks.value.push(newTask.value);
-    newTask.value = "";
-  }
-};
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-};
-
-onMounted(async () => {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = await response.json();
-    tasks.value = data.map((task) => task.title);
-  } catch (error) {
-    console.log(error);
-  }
-});
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 <template>
-  <h1>Vue Jobs</h1>
-  <p>{{ name }}</p>
-
-  <p v-if="status === 'active'">User is Active</p>
-  <p v-else-if="status === 'pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input id="newTask" v-model="newTask" type="text" name="newTask" />
-    <button type="submit">Submit</button>
-  </form>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="(tas, index) in tasks" :key="tas">
-      <span>{{ tas }}</span>
-      <button @click="deleteTask(index)">X</button>
-    </li>
-  </ul>
-
-  <!-- Any of the below two works -->
-  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-  <button @click="toggleStatus">Change Status</button>
+  <Navbar />
+  <Hero
+    title="Become a Vue Dev!"
+    subtitle="Find the Vue job that fits your skills and needs"
+  />
+  <HomeCards />
+  <JobListings :limit="5" :showButton="true" />
 </template>
